8bac5285c520f3eef557117884d652ecd186a134
Kevin Mulkearn <kevinmulkearn@gmail.com>
2018-03-04 00:16:20 +0000

    Phyllotaxis drawer menu added.

--- app/src/main/java/com/mulkearn/kevin/patterns/PhyllotaxisActivity.java
+++ app/src/main/java/com/mulkearn/kevin/patterns/PhyllotaxisActivity.java
@@ -11,7 +11,9 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -57,6 +59,18 @@ public class PhyllotaxisActivity extends AppCompatActivity
         max_value_view = (TextView) findViewById(R.id.max_value_view);
         size_value_view = (TextView) findViewById(R.id.size_value_view);
         angle_value_view = (TextView) findViewById(R.id.angle_value_view);
+
+
+        //Toolbar and drawer setup
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //Get window size
         DisplayMetrics dm = new DisplayMetrics();
@@ -115,6 +129,16 @@ public class PhyllotaxisActivity extends AppCompatActivity
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     public void drawPhyllo(){
